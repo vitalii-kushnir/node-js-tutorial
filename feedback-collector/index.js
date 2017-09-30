8fdd3d44a2c42561f25d26e370a0cb9ccfab8cf3
Vitalii Kushnir <kushnirv88@gmail.com>
2017-09-30 09:05:18 +0200

    Authentication with Google OAuth

--- feedback-collector/index.js
+++ feedback-collector/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStartery = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({
-        hi: 'hello'
-    })
-});
+passport.use(new GoogleStartery({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+}));
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
