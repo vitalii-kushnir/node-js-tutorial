c1afbd786d359c9d1b6f6825c5903d19db77c186
Vitalii Kushnir <kushnirv88@gmail.com>
2017-10-04 20:45:34 +0200

    Developing the Client Side

--- feedback-collector/services/passport.js
+++ feedback-collector/services/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const GoogleStartery = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 
 const keys = require('../config/keys');
@@ -16,20 +16,17 @@ passport.deserializeUser((id, done) => {
         });
 });
 
-passport.use(new GoogleStartery({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: "true"
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({googleId: profile.id})
-        .then((existingUser) => {
-            if (existingUser) {
-                done(null, existingUser);
-            } else {
-                new User({googleId: profile.id})
-                    .save()
-                    .then(user => done(null, user));
-            }
-        });
-}));
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        proxy: "true"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId: profile.id});
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+        const user = await new User({googleId: profile.id}).save();
+        done(null, user);
+    }));
