7fced2c2292cbe399aff866fac2497900311fd18
Vitalii Kushnir <vk@tenios.de>
2017-10-12 16:25:41 +0200

    Handling Webhook Data

--- feedback-collector/routes/surveyRoutes.js
+++ feedback-collector/routes/surveyRoutes.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const Path = require('path-parser');
+const {URL} = require('url');
 
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -9,7 +12,7 @@ const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
 
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
     });
 
@@ -35,7 +38,42 @@ module.exports = (app) => {
         } catch (err) {
             res.status(422).send(err);
         }
+    });
 
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
+        _.chain(req.body)
+            .map(({email, url}) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return {
+                        email,
+                        surveyId: match.surveyId,
+                        choice: match.choice,
+                    };
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({surveyId, email, choice}) => {
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: {email: email, responded: false}
+                        }
+                    },
+                    {
+                        $inc: {[choice]: 1},
+                        $set: {'recipients.$.responded': true},
+                        lastResponded: new Date()
+                    }
+                ).exec();
+            })
+            .value();
+
+        res.send({});
 
     });
 };
